b98a0d756378f4e2944f620b4f6745803938eced
Nadee95 <nadeekacsuop@gmail.com>
2019-07-03 03:38:44 +0530

    home page done

--- src/app/cargoservices/cargoservices.page.ts
+++ src/app/cargoservices/cargoservices.page.ts
@@ -10,19 +10,22 @@ export class CargoservicesPage implements OnInit {
     {
       id: "01",
       name: "Service Provider #01",
-      image: "../assets/images/s1.jpg"
+      image: "../assets/images/s1.jpg",
+      rating:"88"
     },
     {
       id: "02",
       name: "Service Provider #02",
-      image: "../assets/images/s2.jpg"
+      image: "../assets/images/s2.jpg",
+      rating:"12"
     },
     {
       id: "03",
       name: "Service Provider #03",
-      image: "../assets/images/s3.jpg"
+      image: "../assets/images/s3.jpg",
+      rating:"24"
     },
-    { id: "04", name: "Service Provider #04", image: "../assets/images/s4.jpg" }
+    { id: "04", name: "Service Provider #04", image: "../assets/images/s4.jpg",rating:"56" }
   ];
 
   constructor() {}
